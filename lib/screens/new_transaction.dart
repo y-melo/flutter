87d894bd2326e8597b9ef7f4a8e41a248fb21985
ymelo <vieira.yurim@outlook.com>
2020-07-31 13:44:06 -0300

    fix input bug + focus change

--- lib/screens/new_transaction.dart
+++ lib/screens/new_transaction.dart
@@ -10,22 +10,22 @@ class NewTransaction extends StatefulWidget {
 
 class _NewTransactionState extends State<NewTransaction> {
   @override
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
+  void _submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+    print("Title: $enteredTitle | Amount: $enteredAmount");
+    // if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    //   return;
+    // }
+    widget.addTx(enteredTitle, enteredAmount);
+  }
+
   Widget build(BuildContext context) {
-    final titleController = TextEditingController();
-    final amountController = TextEditingController();
-
-    void _submitData() {
-      final enteredTitle = titleController.text;
-      final enteredAmount = double.parse(amountController.text);
-      print("Title: $enteredTitle | Amount: $enteredAmount");
-      if (enteredTitle.isEmpty || enteredAmount <= 0) {
-        return;
-      }
-      widget.addTx(enteredTitle, enteredAmount);
-    }
-
     return Card(
-      elevation: 2,
+      elevation: 5,
       child: Container(
         margin: EdgeInsets.all(5),
         padding: EdgeInsets.all(10),
@@ -33,19 +33,21 @@ class _NewTransactionState extends State<NewTransaction> {
           crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             TextField(
+              autofocus: true,
               decoration: InputDecoration(labelText: "Title"),
               controller: titleController,
-              onSubmitted: (_) => _submitData(),
+              textInputAction: TextInputAction.next,
+              onSubmitted: (_) => FocusScope.of(context).nextFocus(),
               // onChanged: (str) => titleInput = str
             ),
             TextField(
-              keyboardType: TextInputType.numberWithOptions(
-                  decimal: true), // Required for IOS to see decimals
-              onSubmitted: (_) => _submitData(),
               decoration: InputDecoration(
                 labelText: "Amount",
               ),
               controller: amountController,
+              keyboardType: TextInputType.numberWithOptions(
+                  decimal: true), // Required for IOS to see decimals
+              onSubmitted: (_) => _submitData(),
               // onChanged: (str) => amountInput = str
             ),
             FlatButton(
